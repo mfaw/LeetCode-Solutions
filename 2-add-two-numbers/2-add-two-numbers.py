335ade1781d9c666e5d861c670c058f4f5c1d75e
Mariam Fawzi <64710994+mfaw@users.noreply.github.com>
2022-07-02 00:14:15 +0200

    Time: 78 ms (80.51%), Space: 14 MB (43.46%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -9,22 +9,14 @@ class Solution:
         head = ListNode()
         curr = head
         val = 0
-        while (l1 != None or l2!= None or carry != 0):
-            if l1:
-                l1_val = l1.val
-            else: l1_val = 0
-            if l2:
-                l2_val = l2.val
-            else: l2_val = 0
+        while (l1  or l2 or carry):
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
             sum = l1_val + l2_val + carry
             newList = ListNode(sum%10)
             carry = sum // 10
-            if l1:
-                l1 = l1.next
-            else: None
-            if l2:
-                l2 = l2.next
-            else: None
+            l1 = l1.next if l1 else 0
+            l2 = l2.next if l2 else 0
             curr.next = newList
             curr = newList
         return head.next
